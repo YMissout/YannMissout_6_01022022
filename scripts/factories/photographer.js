a165c4d5f3093f8ba4bfb928f09a1fe8da1e8878
Yann <ymissout@gmail.com>
2022-03-01 17:00:20 +0100

    refont (photographer.js + index.js)

---
+++ scripts/factories/photographer.js
@@ -0,0 +1,26 @@
+function photographerFactory(data) {
+  const { name, id, city, country, tagline, price, portrait } = data;
+
+  const picture = `assets/photographers/${portrait}`;
+
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    img.setAttribute("src", picture);
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    const h3 = document.createElement("h3");
+    h3.textContent = city + ", " + country;
+    const h4 = document.createElement("h4");
+    h4.textContent = tagline;
+    const h5 = document.createElement("h5");
+    h5.textContent = price + "€/jour";
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(h3);
+    article.appendChild(h4);
+    article.appendChild(h5);
+    return article;
+  }
+  return { name, id, city, country, tagline, price, picture, getUserCardDOM };
+}
---
+++ scripts/pages/index.js
@@ -0,0 +1,26 @@
+const getPhotographers = async () => {
+  await fetch("./data/photographers.json")
+    .then((res) => res.json())
+    .then((data) => (photographers = data.photographers));
+  return {
+    photographers: [...photographers],
+  };
+};
+
+async function displayData(photographers) {
+  const photographersSection = document.querySelector(".photographer_section");
+
+  photographers.forEach((photographer) => {
+    const photographerModel = photographerFactory(photographer);
+    const userCardDOM = photographerModel.getUserCardDOM();
+    photographersSection.appendChild(userCardDOM);
+  });
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  displayData(photographers);
+}
+
+init();
